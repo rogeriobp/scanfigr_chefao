d61e59d1c367b6945475e0f643475d88e5cddee3
rogeriobp <rogeriobp@gmail.com>
2019-11-01 22:38:37 -0300

    retiradas tags vazias do scss

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,12 +2,12 @@ function getUrl() {
     var url = document.getElementById("urlInput").value;
 
     fetch("https://whatcms.org/APIEndpoint/Detect?key=3270ef043ff53e86c3f7123ab4d2b9177d0bbe2d80a485167db6a1f4a1bbeab581efab&url=" + url)
-        .then(response => response.json())
-        .then(json => {
-            if (json.result.code === 200) {
-                console.log(json);
+        .then(response => response.jsonp())
+        .then(jsonp => {
+            if (jsonp.result.code === 200) {
+                console.log(jsonp);
             } else {
-                console.log(json);
+                console.log(jsonp);
             }
         });
 }
