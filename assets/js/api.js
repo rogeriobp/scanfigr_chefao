5b2f66fdb0e7de4d2919f9b12fa4ffa62f7c10b7
rogeriobp <rogeriobp@gmail.com>
2019-11-01 23:53:30 -0300

    JS validação site

--- assets/js/api.js
+++ assets/js/api.js
@@ -11,3 +11,33 @@ function getUrl() {
             }
         });
 }
+
+function submitResult() {
+    document.getElementById("urlInput").style.backgroundColor = '#B4EAD5';
+    document.getElementById("scanfigr-button").innerHTML = '<i class="fas fa-check"></i>';
+
+    var validado = document.createElement("h3");
+    validado.textContent = 'Excelente! Detectamos que o seu site está rodando o Wordpress 4.72';
+    document.getElementById('form-elements').appendChild(validado);
+
+    var validadoDiv = document.createElement("div");
+    validadoDiv.className = 'validado-div';
+
+    var validadoDivh2 = document.createElement("h2");
+    validadoDivh2.textContent = "Ótimo! Falta pouco..."
+    validadoDiv.appendChild(validadoDivh2);
+
+    var validadoDivP = document.createElement("p");
+    validadoDivP.textContent = "Agora, só precisamos de mais alguns dados para enviarmos o relatório. Não enviamos qualquer tipo de  spam, somente solucões, e em até 48 horas o seu relatório está pronto!"
+    validadoDiv.appendChild(validadoDivP);
+
+    var validadoDivForm = document.createElement("div");
+    validadoDivForm.className = "form-group";
+    validadoDivForm.id = "validadoDivForm";
+    validadoDiv.appendChild(validadoDivForm);
+
+    document.getElementById('validado-texto').appendChild(validadoDiv);
+    document.getElementById('validadoDivForm').innerHTML = "<label for='validadoDivFormNome'>Qual seu nome?</label><input type='text' class='form-control' id='validadoDivFormNome' aria-describedby='validadoNomeInput' placeholder='Fulano da Silva Sauro'> <label for='validadoDivFormEmail'>Qual email podemos enviar o relatório?</label><input type='email' class='form-control' id='validadoDivFormEmail' aria-describedby='validadoEmailInput' placeholder='fulano@provedor.com.br'> <button type='button' class='btn scanfigr-btn-scan' id='scanfigr-button-scan' >Scaneie o meu site <i class='fas fa-chevron-right'></i></button>";
+
+
+}
